711ee87816c996fd6b21272eca58a219649ffbaf
Sebastian Cherny <sebascherny@gmail.com>
2024-09-10 13:31:53 +0200

    Improving app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,27 @@
 import streamlit as st
 from openai import OpenAI
+import boto3
+import json
+from base64 import b64decode
+from langchain_openai import ChatOpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Chatbot with Octo")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple app where you can generate images. "
+    "It's in development so for now you need to enter the AWS credentials here for it to work."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+aws_access_key_id = st.text_input("AWS_ACCESS_KEY_ID", type="password")
+aws_secret_access_key = st.text_input("AWS_SECRET_ACCESS_KEY", type="password")
+aws_session_token = st.text_input("AWS_SESSION_TOKEN", type="password")
+
+if not (aws_access_key_id and aws_secret_access_key and aws_session_token):
+    st.info("Please add the AWS credentials.", icon="🗝️")
 else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
     if "messages" not in st.session_state:
@@ -38,19 +40,41 @@ else:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
+        payload = {
+            "prompt": prompt,
+        }
 
         # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+        RUN_MESSAGE = False
+        if RUN_MESSAGE:
+            # Create an OpenAI client.
+            client = OpenAI(api_key="") # Use ChatOpenAI
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            with st.chat_message("assistant"):
+                response = st.write_stream(stream)
+        else:
+            session = boto3.session.Session(
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                aws_session_token=aws_session_token,
+            )
+            sm_runtime = session.client("sagemaker-runtime", region_name="us-east-1")
+            response = sm_runtime.invoke_endpoint(
+                EndpointName="my-super-octoai-sdxl",
+                ContentType="application/json", Body=json.dumps(payload))
+            response_json = json.loads(response["Body"].read().decode())
+            decoded_image = b64decode(response_json["images"][0]["image_b64"])
+            with open('img.jpg','wb') as f:
+                f.write(decoded_image)
+            response = decoded_image
         st.session_state.messages.append({"role": "assistant", "content": response})
